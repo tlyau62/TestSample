96290ec5eb724183f7209fa217c3bc053ca0d1a4
Tim Yau <tyautl@nexify.com.hk>
2020-07-14 18:35:06 +0800

    First

---
+++ src/IssacPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="template">
+    <p>Hello Issac, {{msg}}</p>
+    <p>{{config}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["config"],
+  data() {
+    return {
+      msg: "world"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.template {
+  background-color: cadetblue;
+}
+</style>
